4382c19b106725d3041321cada0339de53ca2e53
Sharjeel Hussain <sharjeelhusainarain@gmail.com>
2026-06-17 13:48:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 import threading
 import logging
 
+from app import app
+
 logging.basicConfig(
     level = logging.INFO,
     format = "%(asctime)s [%(levelname)s] %(name)s. %(message)s"
